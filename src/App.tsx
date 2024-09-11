2a7f6b21c9a2da045e5fec2a490ade5349c948bb
Dmitry Deshko <dmitri.desh@gmail.com>
2024-09-11 18:15:21 +0300

    feature: Init Project with new template

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import './App.css';
-import {Header} from "./layout/header/Header";
-import {Main} from "./layout/sections/main/Main";
-import {Projects} from "./layout/sections/projects/Projects";
-import {About} from "./layout/sections/about/About";
+import styled from 'styled-components';
 
 
 function App() {
     return (
         <div className="App">
-            <Header/>
-            <Main/>
-            <Projects/>
-            <About/>
+            <Title>Welcome to IT-INCUBATOR</Title>
         </div>
     );
 }
 
 export default App;
+
+const Title = styled.h1`
+    font-size: 1.5em;
+    text-align: center;
+    color: #e91e63;
+`;
--- src/components/SectionTitle.ts
+++
@@ -1,6 +0,0 @@
-import styled from "styled-components";
-
-export const SectionTitle = styled.h2`
-    color: red;
-    text-align: center;
-`;
